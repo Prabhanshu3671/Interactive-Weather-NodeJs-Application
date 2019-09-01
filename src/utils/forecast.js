2292f3d6c1b3d666f857a994bae60d203f54554a
unknown <prabhanshujaiswal123@gmail.com>
2019-09-01 14:24:40 +0530

    Add maximum temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
     }else if(body.error) {
       callback('Unable to find location', undefined);
     }else {
-      callback(undefined, `${body.daily.data[0].summary}It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+      callback(undefined, `${body.daily.data[0].summary} Maximum Temperature-${body.daily.data[0].temperatureHigh} degree, Current temperature-${body.currently.temperature} degree. There is a ${body.currently.precipProbability}% chance of rain.`)
     }
   });
 }; 
